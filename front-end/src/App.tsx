7bb9f5be3d3b89193f9b18fa76b223c757101ff2
amrMagdy <amroraker@gmail.com>
2023-05-06 21:49:18 +0200

    Adding the list of the products

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Products } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <header>SuperStore</header>
+      <main>
+        <ul>
+          {Products.map((product) => (
+            <li key={product.slug}>
+              <img
+                className='imgP'
+                src={product.image}
+                alt={product.name}
+              />
+
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+
+          ))}
+        </ul>
+      </main>
+      <footer>All right reserved.</footer>
     </>
   )
 }
