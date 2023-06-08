df029aace89ebb36bd92b1370d8b66af9da9b8e6
amrMagdy <amroraker@gmail.com>
2023-06-08 18:13:24 +0200

    fixing issue and update ForgotPassword Page

--- front-end/src/pages/Contact.tsx
+++ front-end/src/pages/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
     <>
       <Meta title="Contact Us" />
       <BreadCrumb title="Contact Us" />
-      <Container className="contact py-5 home-wrapper-2">
+      <Container class1="contact py-5 home-wrapper-2">
         <div className="col-12">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d707.2604415311392!2d31.717642718946454!3d30.293237142755455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f8075f7c0ecadb%3A0xc54596c9cf9f7c82!2s10th%20of%20Ramadan%20City%2C%20Ash%20Sharqia%20Governorate!5e0!3m2!1sen!2seg!4v1686083271823!5m2!1sen!2seg"
--- front-end/src/pages/ForgotPassword.tsx
+++ front-end/src/pages/ForgotPassword.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Meta from '../components/Meta';
 import BreadCrumb from '../components/BreadCrumb';
 import { Link } from 'react-router-dom';
 import LoadingBox from '../components/LoadingBox';
 import { Form } from 'react-bootstrap';
+import Container from '../components/Container';
 
 const ForgotPassword = () => {
   const [isLoading, setIsLoading] = useState();
@@ -11,42 +12,37 @@ const ForgotPassword = () => {
     <>
       <Meta title="Contact Us" />
       <BreadCrumb title="Contact Us" />
-      <div className="auth-wrapper py-5 home-wrapper-2">
-        <div className="container-xxl">
-        <div className="row">
-          <div className="col-12">
-            <div className="auth-card">
-              <h3 className="text-center mb-3">Reset Your Password</h3>
-              <p className='mt-2 mb-3 text-center'>We will send send you an email to reset your password</p>
-              <Form onSubmit={isLoading}>
-                <Form.Group className="mb-3" controlId="email">
-                  <Form.Control
-                    type="email"
-                    placeholder="Email"
-                    className="form-input"
-                    required
-                    onChange={(e) => {e}}
-                  />
-                </Form.Group>
-                <div className="flex-column d-flex align-items-center gap-3">
-                  <button className="button" disabled={isLoading} type="submit">
-                    Submit
-                  </button>
+      <Container class1="auth-wrapper py-5 home-wrapper-2">
+        <div className="col-12">
+          <div className="auth-card">
+            <h3 className="text-center mb-3">Reset Your Password</h3>
+            <p className="mt-2 mb-3 text-center">
+              We will send send you an email to reset your password
+            </p>
+            <Form onSubmit={isLoading}>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  className="form-input"
+                  required
+                  onChange={(e) => {
+                    e;
+                  }}
+                />
+              </Form.Group>
+              <div className="flex-column d-flex align-items-center gap-3">
+                <button className="button" disabled={isLoading} type="submit">
+                  Submit
+                </button>
 
-                  <Link
-                    
-                    to="/login"
-                  >
-                    Cancel
-                  </Link>
-                  {isLoading && <LoadingBox />}
-                </div>
-              </Form>
-            </div>
+                <Link to="/login">Cancel</Link>
+                {isLoading && <LoadingBox />}
+              </div>
+            </Form>
           </div>
         </div>
-        </div>
-      </div>
+      </Container>
     </>
   );
 };
