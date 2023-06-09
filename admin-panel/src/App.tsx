b30bfb4fd11d4db3f6ce113b8fec43cad607216e
amrMagdy <amroraker@gmail.com>
2023-06-09 13:06:18 +0200

    starter code for dashboard items

--- admin-panel/src/App.tsx
+++ admin-panel/src/App.tsx
@@ -5,6 +5,16 @@ import MainLayout from './components/MainLayout'
 import Login from './pages/Login'
 import Resetpasword from './pages/Resetpasword'
 import Forgotpassword from './pages/Forgotpassword'
+import Enquiries from './pages/Enquiries'
+import Bloglist from './pages/Bloglist'
+import Orders from './pages/Orders'
+import Customers from './pages/Customers'
+import Colorlist from './pages/Colorlist'
+import Categorieslist from './pages/Categorieslist'
+import Brandlist from './pages/Brandlist'
+import Productlist from './pages/Productlist'
+import Couponlist from './pages/Couponlist'
+import Blogcatlist from './pages/Blogcatlist'
 
 function App() {
 
@@ -16,6 +26,16 @@ function App() {
         <Route path="/forgot-password" element={<Forgotpassword />}/>
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />}/>
+          <Route path='enquiries' element={<Enquiries />}/>
+          <Route path='blog-list' element={<Bloglist />}/>
+          <Route path='orders' element={<Orders />}/>
+          <Route path='customers' element={<Customers />}/>
+          <Route path='blog-category-list' element={<Blogcatlist />}/>
+          <Route path='list-color' element={<Colorlist />}/>
+          <Route path='list-category' element={<Categorieslist />}/>
+          <Route path='list-brand' element={<Brandlist />}/>
+          <Route path='list-product' element={<Productlist />}/>
+          <Route path='coupon-list' element={<Couponlist />}/>
         </Route>
       </Routes>
 
---
+++ admin-panel/src/pages/Customers.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  product: number;
+  status: string;
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'SNo',
+    dataIndex: 'key',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Product',
+    dataIndex: 'product',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+  },
+];
+
+const data1: DataType[] = [];
+for (let i = 0; i < 46; i++) {
+  data1.push({
+    key: i,
+    name: `Edward King ${i}`,
+    product: 32,
+    status: `London, Park Lane no. ${i}`,
+  });
+}
+
+
+const Customers = () => {
+  return (
+    <div>
+      <h3 className="mb-4">Blogs List</h3>
+      <div>
+      <Table columns={columns} dataSource={data1} />
+      </div>
+    </div>
+  )
+}
+
+export default Customers;
