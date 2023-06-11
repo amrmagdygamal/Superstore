1ae140a76157cc049e91012a4b82b2f7b57ed0ce
amrMagdy <amroraker@gmail.com>
2023-06-11 23:09:26 +0200

    implementing delete brand fuctionality

--- admin-panel/src/pages/Brandlist.tsx
+++ admin-panel/src/pages/Brandlist.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../app/store';
-import { getBrands } from '../features/brand/brandSlice';
+import { deleteBrand, getBrands } from '../features/brand/brandSlice';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { BiEdit } from 'react-icons/bi';
 import { AiFillDelete } from 'react-icons/ai';
 import { resetState } from '../features/customers/customerSlice';
+import CustomModal from '../components/CustomeModel';
 
 const columns: any = [
   {
@@ -28,10 +29,22 @@ const columns: any = [
 ];
 
 const Brandlist = () => {
+  const [open, setOpen] = useState(false);
+  const [brandId, setBrandId] = useState("");
+
+  const showModel = (e: string) => {
+    setOpen(true);
+    setBrandId(e);
+  };
+  
+  const hideModel = () => {
+    setOpen(false);
+  };
+
   const dispatch: AppDispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(resetState())
+    dispatch(resetState());
     dispatch(getBrands());
   }, []);
 
@@ -39,21 +52,33 @@ const Brandlist = () => {
 
   const data1: any = [];
   for (let i = 0; i < brandState.length; i++) {
-      data1.push({
-        key: i + 1,
-        name: brandState[i].name,
-        action: (
-          <>
-            <Link to={`/admin/brand/${brandState[i]._id}`} className="fs-3 text-dark">
-              <BiEdit />
-            </Link>
-            <Link className="ms-3 fs-3 text-danger" to="/">
-              <AiFillDelete />
-            </Link>
-          </>
-        ),
-      });
+    data1.push({
+      key: i + 1,
+      name: brandState[i].name,
+      action: (
+        <>
+          <Link
+            to={`/admin/brand/${brandState[i]._id}`}
+            className="fs-3 text-dark"
+          >
+            <BiEdit />
+          </Link>
+          <button className="ms-3 fs-3 text-danger bg-transparent border-0" onClick={() => showModel(brandState[i]._id)}>
+            <AiFillDelete />
+          </button>
+        </>
+      ),
+    });
   }
+
+  const handleDelete = (e: string) => {
+    dispatch(deleteBrand(e));
+    setTimeout(() => {
+      dispatch(getBrands())
+    }, 100);
+    setOpen(false);
+  }
+
 
   return (
     <div>
@@ -61,6 +86,14 @@ const Brandlist = () => {
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        hideModal={hideModel}
+        open={open}
+        performAction={() => {
+          handleDelete(brandId);
+        }}
+        title="Are you sure you want to delete this brand?"
+      />
     </div>
   );
 };
